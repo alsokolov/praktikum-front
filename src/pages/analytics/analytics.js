61a8042ba5597964f14a3076c77de1a1a4b13305
Alexander Sokolov <asokolov.cryolite@gmail.com>
2020-03-28 15:37:25 +0300

    delete junk files

--- src/pages/analytics/analytics.js
+++ src/pages/analytics/analytics.js
@@ -3,9 +3,3 @@ import '../../vendor/fonts/roboto-regular.woff';
 import '../../vendor/fonts/roboto-medium.woff';
 import '../../vendor/fonts/robotoslab-regular.woff';
 import '../../vendor/fonts/sourcesanspro-regular.woff';
-(function () {
-  function init() {
-    console.log(11);
-    }
-  document.addEventListener("DOMContentLoaded", init);
-})();
